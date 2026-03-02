30bf812e8a4deb71d5e8cd5bc6dcd2517b6504a9
JR-Fortran <Johannesrosengren99@gmail.com>
2026-03-02 19:57:49 +0000

    Add files via upload

---
+++ utils.f95
@@ -0,0 +1,96 @@
+! -----------------------------------------------------------------------------
+! utils.f95
+!
+! Simple utility module for reading user input. So that the main file does not
+! need to be edited for changing the most common parameters.
+!
+! If you are generating multiple images and dont want to input the same parameters
+! repeatedly, you can modify the default values in this module.
+!
+! Key parameters:
+!   nx, ny      : output image resolution in pixels
+!   r_obs       : observer radius (Schwarzschild r)
+!   theta_deg   : observer polar angle in degrees
+!   phi_deg     : observer azimuth in degrees
+!   fovx_deg    : horizontal field of view in degrees
+!   answer      : character answer from the user (y/n)
+! -----------------------------------------------------------------------------
+
+MODULE utils
+
+    use, intrinsic :: iso_fortran_env, only: wp => real64
+    implicit none
+    private
+    public :: read_user_input
+
+CONTAINS
+
+    ! subroutine for reading user input
+    subroutine read_user_input(nx, ny, r_obs, theta_deg, phi_deg, fovx_deg)
+        ! Declaring output variables
+        integer, intent(out) :: nx, ny
+        real(wp), intent(out) :: r_obs, theta_deg, phi_deg, fovx_deg
+
+        ! Declaring local variables
+        character(len=1) :: answer
+
+        ! Asking the user if they want to use the default resolution
+        write(*,'(A)', ADVANCE='NO') "Do you want to use the default resolution? (4k) (y/n): "
+        read(*,*) answer
+        if (answer == 'y') then
+            nx = 4096                   ! Default width edit these if you want to change the default resolution
+            ny = 2160                   ! Default height edit these if you want to change the default resolution
+        else
+            write(*,'(A)', ADVANCE='NO') "Enter the number of pixels along the x-axis: "
+            read(*,*) nx
+            write(*,'(A)', ADVANCE='NO') "Enter the number of pixels along the y-axis: "
+            read(*,*) ny
+        end if
+
+        ! Asking the user if they want to use the default camera orientation
+        write(*,'(A)', ADVANCE='NO') "Do you want to use the default camera orientation? (theta = 80, phi = 0) (y/n): "
+        read(*,*) answer
+        if (answer == 'y') then
+            theta_deg = 80.0_wp         ! Default polar angle edit these if you want to change the default
+            phi_deg = 0.0_wp            ! Default azimuthal angle edit these if you want to change the default
+        else
+            write(*,'(A)', ADVANCE='NO') "Enter the polar angle in degrees: "
+            read(*,*) theta_deg
+            write(*,'(A)', ADVANCE='NO') "Enter the azimuthal angle in degrees: "
+            read(*,*) phi_deg
+        end if
+
+        ! Asking the user if they want to use the default field of view and distance
+        write(*,'(A)', ADVANCE='NO') "Do you want to use the default field of view and distance? (fovx_deg = 40, r_obs = 150) (y/n): "
+        read(*,*) answer
+        if (answer == 'y') then
+            fovx_deg = 40.0_wp            ! Default field of view edit these if you want to change the default
+            r_obs = 150.0_wp              ! Default distance edit these if you want to change the default
+        else
+            write(*,'(A)', ADVANCE='NO') "Enter the field of view in degrees: "
+            read(*,*) fovx_deg
+            write(*,'(A)', ADVANCE='NO') "Enter the distance from the observer to the center: "
+            read(*,*) r_obs
+        end if
+
+        write(*,*) "Settings:"
+        write(*,*) "  nx = ", nx
+        write(*,*) "  ny = ", ny
+        write(*,*) "  theta_deg = ", theta_deg
+        write(*,*) "  phi_deg = ", phi_deg
+        write(*,*) "  fovx_deg = ", fovx_deg
+        write(*,*) "  r_obs = ", r_obs
+
+        ! Asking the user if they want to proceed with these settings
+        write(*,'(A)', ADVANCE='NO') "Do you want to proceed with these settings? (y/n): "
+        read(*,*) answer
+        if (answer == 'y') then
+            write(*,*) "Proceeding with settings..."
+        else
+            write(*,*) "Exiting..."
+            stop
+        end if
+
+    end subroutine read_user_input
+
+END MODULE utils
