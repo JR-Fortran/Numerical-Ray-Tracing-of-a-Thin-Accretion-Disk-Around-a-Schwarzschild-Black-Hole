97f9913025774fd5f2478d47d922151ddeae568e
JR-Fortran <Johannesrosengren99@gmail.com>
2026-03-03 19:36:49 +0000

    Enhance read_user_input with temperature parameter
    Added temperature input option to user settings.
    And large resolution warning.

--- utils.f95
+++ utils.f95
@@ -26,13 +26,18 @@ MODULE utils
 CONTAINS
 
     ! subroutine for reading user input
-    subroutine read_user_input(nx, ny, r_obs, theta_deg, phi_deg, fovx_deg)
+    subroutine read_user_input(nx, ny, r_obs, theta_deg, phi_deg, fovx_deg, T0)
         ! Declaring output variables
         integer, intent(out) :: nx, ny
-        real(wp), intent(out) :: r_obs, theta_deg, phi_deg, fovx_deg
+        real(wp), intent(out) :: r_obs, theta_deg, phi_deg, fovx_deg, T0
 
         ! Declaring local variables
         character(len=1) :: answer
+
+        ! Print a welcome message
+        write(*,'(A)') "-----------------------------------------------------------------"
+        write(*,'(A)') "       Welcome to Schwarzschild accretion disk renderer!"
+        write(*,'(A)') "-----------------------------------------------------------------"
 
         ! Asking the user if they want to use the default resolution
         write(*,'(A)', ADVANCE='NO') "Do you want to use the default resolution? (4k) (y/n): "
@@ -48,6 +53,7 @@ CONTAINS
         end if
 
         ! Asking the user if they want to use the default camera orientation
+        write(*,'(A)') "-----------------------------------------------------------------"
         write(*,'(A)', ADVANCE='NO') "Do you want to use the default camera orientation? (theta = 80, phi = 0) (y/n): "
         read(*,*) answer
         if (answer == 'y') then
@@ -61,6 +67,7 @@ CONTAINS
         end if
 
         ! Asking the user if they want to use the default field of view and distance
+        write(*,'(A)') "-----------------------------------------------------------------"
         write(*,'(A)', ADVANCE='NO') "Do you want to use the default field of view and distance?" //&
                                         "(fovx_deg = 40, r_obs = 150) (y/n): "
         read(*,*) answer
@@ -74,13 +81,40 @@ CONTAINS
             read(*,*) r_obs
         end if
 
+        ! Asking the user if they want to use the default temperature
+        write(*,'(A)') "-----------------------------------------------------------------"
+        write(*,'(A)', ADVANCE='NO') "Do you want to use the default temperature? (y/n): "
+        read(*,*) answer
+        if (answer == 'y') then
+            T0 = 9000.0_wp            ! Default temperature edit this if you want to change the default
+        else
+            write(*,'(A)', ADVANCE='NO') "Enter the temperature: "
+            read(*,*) T0
+        end if
+
+        ! Printing the settings
+        write(*,'(A)') "================================================================="
         write(*,*) "Settings:"
+        write(*,'(A)') "-----------------------------------------------------------------"
         write(*,*) "  nx = ", nx
         write(*,*) "  ny = ", ny
+        write(*,'(A)') "-----------------------------------------------------------------"
         write(*,*) "  theta_deg = ", theta_deg
         write(*,*) "  phi_deg = ", phi_deg
+        write(*,'(A)') "-----------------------------------------------------------------"
         write(*,*) "  fovx_deg = ", fovx_deg
         write(*,*) "  r_obs = ", r_obs
+        write(*,'(A)') "-----------------------------------------------------------------"
+        write(*,*) "  T0 = ", T0
+        write(*,'(A)') "-----------------------------------------------------------------"
+
+        ! Warn the user about the resolution
+        if (nx > 4096 .or. ny > 2160) then
+            write(*,'(A)') "-----------------------------------------------------------------"
+            write(*,*) "Warning: it looks like you're using a resolution higher than 4k."
+            write(*,*) "This may take a while to render."
+            write(*,'(A)') "-----------------------------------------------------------------"
+        end if
 
         ! Asking the user if they want to proceed with these settings
         write(*,'(A)', ADVANCE='NO') "Do you want to proceed with these settings? (y/n): "
